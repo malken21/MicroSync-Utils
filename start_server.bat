de50156181f2fa5accfa07d7341d36ae8568c224
Marumasa <77374813+malken21@users.noreply.github.com>
2026-03-07 16:53:33 +0900

    Create start_server.bat

---
+++ start_server.bat
@@ -0,0 +1,8 @@
+@echo off
+chcp 65001 > nul
+set EXE_PATH=.\Unity-GLB-Vehicle-Sync.exe
+set PORT=7777
+set ASSET_URL=http://localhost:3000
+
+echo [起動] サーバー(HOST)モードを開始します...
+start "" "%EXE_PATH%" -mode HOST -port %PORT% -assetUrl %ASSET_URL% -logfile ./server.log
