77a57457ef2ec3102121c6434843aa770a0770e5
malken21 <77374813+malken21@users.noreply.github.com>
2026-03-14 12:53:53 +0900

    Update start_client.ps1

--- start_client.ps1
+++ start_client.ps1
@@ -7,5 +7,4 @@ Show-Header "MicroSync Client"
 $scriptDir = $PSScriptRoot
 $exeName = "Unity-GLB-Vehicle-Sync.exe"
 
-Update-App -scriptDir $scriptDir -repo "malken21/Unity-GLB-Vehicle-Sync" -assetPattern "Windows\.zip$" -outputPath (Join-Path $scriptDir "Windows.zip") -isZip $true
-Start-App -scriptDir $scriptDir -exeName $exeName -port 7777 -logName "client_log.txt"
+Start-App -scriptDir $scriptDir -exeName $exeName -mode CLIENT -serverIp "[IP_ADDRESS]" -port 7777 -logName "client_log.txt"
